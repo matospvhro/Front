e861cd14d5964f38c267fc9e23a74b5d87a94b90
almir matos <almir.matospvh@hotmail.com>
2019-12-07 10:02:04 -0400

    Conexao finalizada

--- Front/Repository/AcessoFB.cs
+++ Front/Repository/AcessoFB.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using FirebirdSql.Data.FirebirdClient;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,64 @@ namespace Front.Repository
 {
     public class AcessoFB
     {
+        private static readonly AcessoFB instanciaFireBird = new AcessoFB();
+
+        //private AcessoFB() { }
+
+        public static AcessoFB GetInstancia()
+        {
+            return instanciaFireBird;
+        }
+
+        public FbConnection GetConexao()
+        {            
+            string conn = ConfigurationManager.ConnectionStrings["FireBirdConnectionString"].ToString();
+            return new FbConnection(conn);
+        }
+
+        public bool TestarConexao()
+        {
+            bool boll = false;
+            using (FbConnection conexaoFireBird = AcessoFB.GetInstancia().GetConexao())
+            {
+                try
+                {
+                    conexaoFireBird.Open();
+                    boll = true;
+                    conexaoFireBird.Close();
+                }
+                catch (Exception)
+                {
+                    return boll = false;
+                }
+            }
+            return boll;
+        }
+
+        //public object BuscarMarca()
+        //{
+        //    using (FbConnection conexaoFireBird = AcessoFB.GetInstancia().GetConexao())
+        //    {
+        //        try
+        //        {
+        //            conexaoFireBird.Open();
+        //            string mSQL = "select gp.id_marca_produto, gp.nome_marca_produto from marcas_produtos gp order by gp.nome_marca_produto";
+        //            FbCommand cmd = new FbCommand(mSQL, conexaoFireBird);
+        //            FbDataAdapter da = new FbDataAdapter(cmd);
+        //            DataTable dt = new DataTable();
+        //            da.Fill(dt);
+        //            return dt;
+        //        }
+
+        //        catch (FbException fbex)
+        //        {
+        //            throw fbex;
+        //        }
+        //        finally
+        //        {
+        //            conexaoFireBird.Close();
+        //        }
+        //    }
+        //}
     }
 }
